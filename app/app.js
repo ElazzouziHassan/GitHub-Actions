9eafdc18db7869623555b87b23901a6ed0ad4825
wizardy <wizardhassan99@gmail.com>
2023-09-08 20:09:53 +0100

    commit

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-const result = (a, b) => {
-  return a > b ? "a is geater than b" : "b greater than a"
-}
+const result = (a, b) => a > b ? "a is geater than b" : "b greater than a"
+
 console.log(result(5,7))
